d24cb138a89b9f544a37e56a0f9f598f360beab8
Evgeny Ulanov <jeny@ulanoff.ru>
2018-10-02 17:41:26 +0300

    UPS

---
+++ University/app/src/main/java/com/example/jake/university/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.jake.university;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by htcvive on 01.10.2018.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

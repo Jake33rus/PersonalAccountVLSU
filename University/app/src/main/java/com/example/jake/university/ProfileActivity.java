9b53d875e34ae5375ad93853a0bfc069370fca0d
Evgeny Ulanov <jeny@ulanoff.ru>
2018-10-07 23:38:45 +0300

    update

---
+++ University/app/src/main/java/com/example/jake/university/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.jake.university;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ProfileActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}

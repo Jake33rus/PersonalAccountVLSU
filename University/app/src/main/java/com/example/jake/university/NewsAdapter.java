b96a87eab6df5b9b24541335f31717357e7448ff
Kubiken <kubiken@yandex.ru>
2018-10-19 21:50:14 +0300

    Now with news

---
+++ University/app/src/main/java/com/example/jake/university/NewsAdapter.java
@@ -0,0 +1,14 @@
+package com.example.jake.university;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class NewsAdapter
+{
+
+}

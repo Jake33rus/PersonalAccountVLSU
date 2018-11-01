8a151099531cc49427f31514b04e57622dc63274
Unknown <jeny@ulanoff.ru>
2018-11-01 22:23:43 +0300

    API

--- University/app/src/main/java/com/example/jake/university/API/Comands.java
+++ University/app/src/main/java/com/example/jake/university/API/Comands.java
@@ -1,95 +1,38 @@
 package com.example.jake.university.API;
 
-/*public class Comands {
-    public static void RegisterMail(String login, String password, String email, String type)
-    {
-        //JObject - в шарпе - библиотека для работы с JSON надо найти аналог на java и прикрутить
-        JObject jObject;
-        try
-        {
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-
-            using (var webClient = new WebClient())
-            {
-                //Аналога для var в java нет, хз че делать!
-                var info = new NameValueCollection
-                {
-                    { "Email", Encryption.Encr(email)},
-                    { "Password", Encryption.Encr(password)},
-                    { "Name",Encryption.Encr( login)},
-                    { "Type",Encryption.Encr( type)}
-                };
-                //Че это за класс тоже хз, подозреваю что то тоже из JSON
-                CredentialCache cc = new CredentialCache();
-                cc.Add(
-                        new Uri(MvcApplication.urlApi + "Mail/GetS/"),
-                        "NTLM",
-
-                        CredentialCache.DefaultNetworkCredentials);
-                webClient.Credentials = cc;
-
-
-                // Посылаем параметры на сервер
-                // Может быть ответ в виде массива байт
-                byte[] responseArray = webClient.UploadValues(MvcApplication.urlApi +
-                        "Mail/GetS/", "POST", info);
-                String resul = Encoding.UTF8.GetString(responseArray);
-                jObject = JObject.Parse("{ 'result':" + resul + "}");
-
-            }
-        }
-        catch (WebException e)
-        {
-            //хз что делать с гребаными потоками
-            System.IO.Stream stream = e.Response.GetResponseStream();
-            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
-            String resul = sr.ReadToEnd();
-            throw new Exception(resul);
-        }
-
-
-    }
-
-
+class Comands {
 
     //вызов процедуры или функции из бд
     //результат в виде массива json
-    public static JArray GetTableArray(String idDb, String nameExec, String paramsList)
+    public static JSONObject GetTableArray(String idDb, String nameExec, String paramsList)
     {
-        JArray jObject = new JArray();
-        try
+        JSONObject jObject = new JSONObject();
+        TripleDES crypter = new TripleDES();
+        idDb = crypter.Encr(idDb);
+        paramsList = crypter.Encr(paramsList);
+        nameExec = crypter.Encr(nameExec);
+
+        WebClient myWebClient = new WebClient();
+        var info = new NameValueCollection
         {
-            idDb = Encryption.Encr(idDb);
-            paramsList = Encryption.Encr(paramsList);
-            nameExec = Encryption.Encr(nameExec);
-
-            WebClient myWebClient = new WebClient();
-            var info = new NameValueCollection
-            {
-                { "idDb", idDb },
-                { "nameExec",  nameExec},
-                { "paramsList",  paramsList},
-
-            };
-            CredentialCache cc = new CredentialCache();
-            cc.Add(
-                    new Uri(MvcApplication.urlApi + "GetTable"),
-                    "NTLM",
-
-                    CredentialCache.DefaultNetworkCredentials);
-            myWebClient.Credentials = cc;
+            { "idDb", idDb },
+            { "nameExec",  nameExec},
+            { "paramsList",  paramsList},
+        };
+        CredentialCache cc = new CredentialCache();
+        cc.Add(
+                new Uri(MvcApplication.urlApi + "GetTable"),
+                "NTLM",
+                CredentialCache.DefaultNetworkCredentials);
+        myWebClient.Credentials = cc;
 
 
-            String result = Encoding.UTF8.GetString(myWebClient.UploadValues(MvcApplication.urlApi + "GetTable", "POST", info));
-            jObject = JArray.Parse(result);
-        }
-        catch (WebException e)
-        {
-            System.IO.Stream stream = e.Response.GetResponseStream();
-            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
-            String resul = sr.ReadToEnd();
-            throw new Exception(resul);
-        }
+        String result = Encoding.UTF8.GetString(myWebClient.UploadValues(MvcApplication.urlApi + "GetTable", "POST", info));
+        jObject = JSONArray.Parse(result);
+
         return jObject;
     }
-}*/
+}
---
+++ University/app/src/main/java/com/example/jake/university/API/TripleDES.java
@@ -0,0 +1,117 @@
+package com.example.jake.university.API;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+public class TripleDES {
+
+    //Метод для шифрования
+    public String Encrypter(String skey,String sdata, String siv) {
+        /*
+        * date - Данные, которые необходимо зашифровать;
+        * key - ключ шифрования;
+        * iv - вектор инициализации для симетричного алгоритма;
+        */
+        byte[] key = skey.getBytes();
+        byte[] data = sdata.getBytes();
+        byte[] iv = siv.getBytes();
+        IvParameterSpec ivParam = new IvParameterSpec(iv);
+        Key deskey = null;
+        StringBuffer hexCiphertext = null;
+        DESedeKeySpec spec;
+        try {
+            spec = new DESedeKeySpec(key);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("desede");
+            deskey = keyFactory.generateSecret(spec);
+
+            Cipher cipher = Cipher.getInstance("desede"+"/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, deskey, ivParam);
+
+            byte[] CipherText = cipher.doFinal(data);
+            hexCiphertext = new StringBuffer();
+            for (int i=0;i<CipherText.length; i++)
+                hexCiphertext.append(Integer.toString((CipherText[i]&0xff)+0x100,16).substring(1));
+
+        }
+        catch(InvalidKeyException ex) {
+            Logger.getLogger(TripleDES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch(NoSuchAlgorithmException ex) {
+            Logger.getLogger(TripleDES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (InvalidKeySpecException ex){
+            Logger.getLogger(TripleDES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (NoSuchPaddingException ex){
+            Logger.getLogger(TripleDES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (IllegalBlockSizeException ex){
+            Logger.getLogger(TripleDES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (BadPaddingException ex){
+            Logger.getLogger(TripleDES.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidAlgorithmParameterException ex) {
+            Logger.getLogger(TripleDES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return new String(hexCiphertext);
+    }
+
+    //метод для дешифровки
+    public String Dencrypt(String sdata)
+    {
+        byte[] key = "".getBytes();
+        byte[] data = sdata.getBytes();
+        byte[] iv = "".getBytes();
+        IvParameterSpec ivParam = new IvParameterSpec(iv);
+        Key deskey = null;
+        StringBuffer hexCiphertext = null;
+        DESedeKeySpec spec;
+        byte[] res = null;
+        try {
+            spec = new DESedeKeySpec(key);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("desede");
+            deskey = keyFactory.generateSecret(spec);
+
+            Cipher cipher = Cipher.getInstance("desede"+"/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, deskey, ivParam);
+
+            res = cipher.doFinal(data);
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return new String(res);
+    }
+    public String Encr(String data)
+    {
+        String skey = "";
+        String siv = "";
+        return Encrypter(skey,data,siv);
+    }
+}

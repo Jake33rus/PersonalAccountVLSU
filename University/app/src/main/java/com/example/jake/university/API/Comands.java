c1844eb73447c13bb48bc07682556c2265a524fd
Unknown <jeny@ulanoff.ru>
2018-11-08 20:32:10 +0300

    Auto stash before merge of "master" and "origin/master"

--- University/app/src/main/java/com/example/jake/university/API/Comands.java
+++ University/app/src/main/java/com/example/jake/university/API/Comands.java
@@ -1,43 +1,30 @@
 package com.example.jake.university.API;
 
-import android.net.Uri;
-
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.Credentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.AuthSchemes;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 
 class Comands {
 
+    public static String urlApi()
+    {   return "http://hqvla7327aw01:8000//api/";   }
     //вызов процедуры или функции из бд
     //результат в виде массива json
     public static JSONObject GetTableArray(String idDb, String nameExec, String paramsList) {
@@ -49,7 +36,11 @@ class Comands {
 
         /*WebClient myWebClient = new WebClient();*/
         HttpClient myWebClient;
+<<<<<<< Updated upstream
         HttpPost post = new HttpPost("http://hqvla7327aw01:8000//api/" + "GetTable");
+=======
+        HttpPost post = new HttpPost(urlApi() + "GetTable");
+>>>>>>> Stashed changes
         List<NameValuePair> info;
 
 

84da76e567e76d3af9980a6505bd526f4b809f93
Unknown <jeny@ulanoff.ru>
2018-12-17 19:53:22 +0300

    Add hz che

--- University/app/src/main/java/com/example/jake/university/fragments/FragmentArrears.java
+++ University/app/src/main/java/com/example/jake/university/fragments/FragmentArrears.java
@@ -4,8 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.jake.university.API.postReq;
 import com.example.jake.university.R;
+import com.example.jake.university.adapter.ArrearsAdapter;
+import com.example.jake.university.adapter.ExamItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +26,9 @@ public class FragmentArrears extends Fragment {
 
     private static final int LAYOUT = R.layout.fragment_arrears;
     private View view;
+    JSONArray arr;
+    JSONObject jobj;
+    postReq comand;
     public static FragmentArrears getInstance() {
         Bundle args = new Bundle();
         FragmentArrears fragment = new FragmentArrears();
@@ -26,6 +40,36 @@ public class FragmentArrears extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
+        ArrearsAdapter adapter = null;
+        try {
+            ArrayList<ExamItem> list = ReadInDB();
+            adapter = new ArrearsAdapter(view.getContext(), R.layout.item_arrears, list);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        ListView lv = (ListView) view.findViewById(R.id.lv_arrears);
+        lv.setAdapter(adapter);
         return view;
+    }
+    protected ArrayList<ExamItem> ReadInDB() throws ExecutionException, InterruptedException, JSONException {
+        jobj = new JSONObject();
+        comand = new postReq();
+        comand.execute("10","A_LKS_GetMarks","0x8001000000027C02").get();
+        arr = comand.getjARRAY();
+        ArrayList<ExamItem> list = new ArrayList();
+        for (int i=0; i<arr.length(); i++) {
+            jobj = arr.getJSONObject(i);
+            String result = jobj.getString("Оценка");
+            if (result == result || result == "не явка") {
+                list.add(new ExamItem(jobj.getString("Наименование дисциплины"),
+                        jobj.getString("Семестр"),
+                        jobj.getString("Тип")));
+            }
+        }
+        return list;
     }
 }
--- University/app/src/main/java/com/example/jake/university/fragments/FragmentUpcomingExams.java
+++ University/app/src/main/java/com/example/jake/university/fragments/FragmentUpcomingExams.java
@@ -6,11 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.example.jake.university.API.postReq;
 import com.example.jake.university.R;
 import com.example.jake.university.adapter.ArrearsAdapter;
 import com.example.jake.university.adapter.ExamItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +26,9 @@ public class FragmentUpcomingExams extends Fragment {
     private static final int LAYOUT = R.layout.fragment_upcoming_exams;
     private View view;
     private ListView lv;
+    JSONArray arr;
+    JSONObject jobj;
+    postReq comand;
     public static FragmentUpcomingExams getInstance() {
         Bundle args = new Bundle();
         FragmentUpcomingExams fragment = new FragmentUpcomingExams();
@@ -31,14 +40,34 @@ public class FragmentUpcomingExams extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
-        lv = (ListView) view.findViewById(R.id.lv_arrears);
-        ArrearsAdapter adapter = new ArrearsAdapter(view.getContext(), R.layout.item_arrears, ReadInDB());
+        lv = (ListView) view.findViewById(R.id.lv_upcom_exams);
+        ArrearsAdapter adapter = null;
+        try {
+            adapter = new ArrearsAdapter(view.getContext(), R.layout.item_arrears, ReadInDB());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         lv.setAdapter(adapter);
         return view;
     }
-    protected ArrayList<ExamItem> ReadInDB()
-    {
+    protected ArrayList<ExamItem> ReadInDB() throws ExecutionException, InterruptedException, JSONException {
+        jobj = new JSONObject();
+        comand = new postReq();
+        comand.execute("10","A_LKS_GetMarks","0x8001000000027C02").get();
+        arr = comand.getjARRAY();
         ArrayList<ExamItem> list = new ArrayList();
+        for (int i=0; i<arr.length(); i++) {
+            jobj = arr.getJSONObject(i);
+            if (jobj.getString("Оценка") == "нет оценки") {
+                list.add(new ExamItem(jobj.getString("Наименование дисциплины"),
+                        jobj.getString("Семестр"),
+                        jobj.getString("Тип")));
+            }
+        }
         return list;
     }
 }

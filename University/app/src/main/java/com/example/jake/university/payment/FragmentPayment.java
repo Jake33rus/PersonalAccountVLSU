ae23d5984c381f34a27191fc63e469730283f23b
Unknown <jeny@ulanoff.ru>
2019-04-05 23:17:44 +0300

    Add payment and paymentInfo fragment

--- University/app/src/main/java/com/example/jake/university/payment/FragmentPayment.java
+++ University/app/src/main/java/com/example/jake/university/payment/FragmentPayment.java
@@ -4,18 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.jake.university.R;
+
+import org.w3c.dom.Text;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class FragmentPayment extends Fragment {
+    TextView payLocation;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_payment, container, false);
+        payLocation = (TextView) view.findViewById(R.id.tvPayLocation);
+        payLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.fragment_container, new FragmentPaymentInfo()).addToBackStack(null).commit();
+            }
+        });
         return view;
     }
 }
